57cc248ca5f91451cd21d8c3b69da88f64ab857f
Jordon de Hoog <jordon.dehoog@gmail.com>
2024-01-30 20:04:18 -0500

    add documentation for extensions

--- material-kolor/src/commonMain/kotlin/com/materialkolor/LocalDynamicMaterialThemeSeed.kt
+++ material-kolor/src/commonMain/kotlin/com/materialkolor/LocalDynamicMaterialThemeSeed.kt
@@ -4,5 +4,9 @@ import androidx.compose.runtime.ProvidableCompositionLocal
 import androidx.compose.runtime.staticCompositionLocalOf
 import androidx.compose.ui.graphics.Color
 
+/**
+ * LocalDynamicMaterialThemeSeed is a [CompositionLocal] that provides the seed color for the
+ * dynamic material theme.
+ */
 public val LocalDynamicMaterialThemeSeed: ProvidableCompositionLocal<Color> =
     staticCompositionLocalOf { error("DynamicMaterialTheme not initialized") }
