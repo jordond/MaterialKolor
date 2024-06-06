11d2185e21059790f22e2ddfe97e8ae868255040
Cuong Tran <cuongtran.tm@gmail.com>
2024-06-07 02:24:52 +0700

    support Amoled's pure dark mode

--- material-kolor/src/commonMain/kotlin/com/materialkolor/DynamicColorScheme.kt
+++ material-kolor/src/commonMain/kotlin/com/materialkolor/DynamicColorScheme.kt
@@ -13,6 +13,7 @@ import com.materialkolor.ktx.toDynamicScheme
  *
  * @param[seedColor] The color to base the scheme on.
  * @param[isDark] Whether the scheme should be dark or light.
+ * @param[isAmoled] Whether the dark scheme is used with Amoled screen (Pure dark).
  * @param[style] The style of the scheme.
  * @param[contrastLevel] The contrast level of the scheme.
  * @param[isExtendedFidelity] Whether to use the extended fidelity color set. See [MaterialDynamicColors].
@@ -21,6 +22,7 @@ import com.materialkolor.ktx.toDynamicScheme
 public fun rememberDynamicColorScheme(
     seedColor: Color,
     isDark: Boolean,
+    isAmoled: Boolean = false,
     style: PaletteStyle = PaletteStyle.TonalSpot,
     contrastLevel: Double = Contrast.Default.value,
     isExtendedFidelity: Boolean = false,
@@ -28,6 +30,7 @@ public fun rememberDynamicColorScheme(
 ): ColorScheme = remember(
     seedColor,
     isDark,
+    isAmoled,
     style,
     contrastLevel,
     isExtendedFidelity,
@@ -36,6 +39,7 @@ public fun rememberDynamicColorScheme(
     dynamicColorScheme(
         seedColor = seedColor,
         isDark = isDark,
+        isAmoled = isAmoled,
         style = style,
         contrastLevel = contrastLevel,
         isExtendedFidelity = isExtendedFidelity,
@@ -48,6 +52,7 @@ public fun rememberDynamicColorScheme(
  *
  * @param[seedColor] The color to base the scheme on.
  * @param[isDark] Whether the scheme should be dark or light.
+ * @param[isAmoled] Whether the dark scheme is used with Amoled screen (Pure dark).
  * @param[style] The style of the scheme.
  * @param[contrastLevel] The contrast level of the scheme.
  * @param[isExtendedFidelity] Whether to use the extended fidelity color set. See [MaterialDynamicColors].
@@ -55,6 +60,7 @@ public fun rememberDynamicColorScheme(
 public fun dynamicColorScheme(
     seedColor: Color,
     isDark: Boolean,
+    isAmoled: Boolean,
     style: PaletteStyle = PaletteStyle.TonalSpot,
     contrastLevel: Double = Contrast.Default.value,
     isExtendedFidelity: Boolean = false,
@@ -64,20 +70,20 @@ public fun dynamicColorScheme(
     val colors = MaterialDynamicColors(isExtendedFidelity)
 
     return ColorScheme(
-        background = colors.background().getColor(scheme),
+        background = if (isDark && isAmoled) Color.Black else colors.background().getColor(scheme),
         error = colors.error().getColor(scheme),
         errorContainer = colors.errorContainer().getColor(scheme),
         inverseOnSurface = colors.inverseOnSurface().getColor(scheme),
         inversePrimary = colors.inversePrimary().getColor(scheme),
         inverseSurface = colors.inverseSurface().getColor(scheme),
-        onBackground = colors.onBackground().getColor(scheme),
+        onBackground = if (isDark && isAmoled) Color.White else colors.onBackground().getColor(scheme),
         onError = colors.onError().getColor(scheme),
         onErrorContainer = colors.onErrorContainer().getColor(scheme),
         onPrimary = colors.onPrimary().getColor(scheme),
         onPrimaryContainer = colors.onPrimaryContainer().getColor(scheme),
         onSecondary = colors.onSecondary().getColor(scheme),
         onSecondaryContainer = colors.onSecondaryContainer().getColor(scheme),
-        onSurface = colors.onSurface().getColor(scheme),
+        onSurface = if (isDark && isAmoled) Color.White else colors.onSurface().getColor(scheme),
         onSurfaceVariant = colors.onSurfaceVariant().getColor(scheme),
         onTertiary = colors.onTertiary().getColor(scheme),
         onTertiaryContainer = colors.onTertiaryContainer().getColor(scheme),
@@ -88,7 +94,7 @@ public fun dynamicColorScheme(
         scrim = colors.scrim().getColor(scheme),
         secondary = colors.secondary().getColor(scheme),
         secondaryContainer = colors.secondaryContainer().getColor(scheme),
-        surface = colors.surface().getColor(scheme),
+        surface = if (isDark && isAmoled) Color.Black else colors.surface().getColor(scheme),
         surfaceTint = colors.surfaceTint().getColor(scheme),
         surfaceBright = colors.surfaceBright().getColor(scheme),
         surfaceDim = colors.surfaceDim().getColor(scheme),
