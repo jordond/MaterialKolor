0a4181ed7d352b66386cd6b0224fc837a385c344
Jordon de Hoog <jordon.dehoog@gmail.com>
2025-05-03 12:15:30 -0400

    fix 2021 colors

--- mcu-upstream/src/test/java/scheme/DynamicSchemeTest.kt
+++ mcu-upstream/src/test/java/scheme/DynamicSchemeTest.kt
@@ -1,11 +1,12 @@
 package scheme
 
 import com.materialkolor.dynamiccolor.ColorSpec
+import com.materialkolor.dynamiccolor.MaterialDynamicColors
 import com.materialkolor.hct.Hct
 import com.materialkolor.scheme.DynamicScheme
 import com.materialkolor.scheme.SchemeTonalSpot
 import io.kotest.matchers.doubles.shouldBeExactly
-import io.kotest.matchers.ints.shouldBeExactly
+import io.kotest.matchers.equals.shouldBeEqual
 import io.kotest.matchers.string.shouldBeEqualIgnoringCase
 import kotlin.test.Test
 
@@ -40,6 +41,116 @@ class DynamicSchemeTest {
         val mkOnPrimary = mkScheme.onPrimaryContainer
         val mcuOnPrimary = mcuScheme.onPrimaryContainer
 
-        mkOnPrimary.toHexString() shouldBeEqualIgnoringCase  mcuOnPrimary.toHexString()
+        mkOnPrimary.toHexString() shouldBeEqualIgnoringCase mcuOnPrimary.toHexString()
     }
+
+    @Test
+    fun testAllColors2021() {
+        testAllColors(ColorSpec.SpecVersion.SPEC_2021)
+    }
+
+    @Test
+    fun testAllColors2025() {
+        testAllColors(ColorSpec.SpecVersion.SPEC_2025)
+    }
+
+    private fun testAllColors(specVersion: ColorSpec.SpecVersion) {
+        val mkSourceColor = Hct.from(131.0, 70.0, 62.0)
+        val mkScheme = SchemeTonalSpot(mkSourceColor, true, 1.0, specVersion)
+
+        val mcuSourceColor = hct.Hct.from(131.0, 70.0, 62.0)
+        val mcuScheme = scheme.SchemeTonalSpot(
+            mcuSourceColor,
+            true,
+            1.0,
+            when (specVersion) {
+                ColorSpec.SpecVersion.SPEC_2021 -> dynamiccolor.ColorSpec.SpecVersion.SPEC_2021
+                ColorSpec.SpecVersion.SPEC_2025 -> dynamiccolor.ColorSpec.SpecVersion.SPEC_2025
+            },
+            dynamiccolor.DynamicScheme.Platform.PHONE,
+        )
+
+        val mkColors = MaterialDynamicColors()
+            .allDynamicColors()
+            .map {
+                val color = it()
+                "${color.name} -> #${color.getArgb(mkScheme).toHexString()}"
+            }
+
+        val mcuColors = dynamiccolor.MaterialDynamicColors()
+            .allInOrder()
+            .map {
+                val color = it()
+                "${color.name} -> #${color.getArgb(mcuScheme).toHexString()}"
+            }
+
+        val incorrect = mkColors.zip(mcuColors).filter { it.first != it.second }.joinToString("\n")
+        incorrect shouldBeEqual ""
+    }
+
+    private fun dynamiccolor.MaterialDynamicColors.allInOrder() = listOf(
+        ::primaryPaletteKeyColor,
+        ::secondaryPaletteKeyColor,
+        ::tertiaryPaletteKeyColor,
+        ::errorPaletteKeyColor,
+        ::neutralPaletteKeyColor,
+        ::neutralVariantPaletteKeyColor,
+        ::background,
+        ::onBackground,
+        ::surface,
+        ::surfaceDim,
+        ::surfaceBright,
+        ::surfaceContainerLowest,
+        ::surfaceContainerLow,
+        ::surfaceContainer,
+        ::surfaceContainerHigh,
+        ::surfaceContainerHighest,
+        ::onSurface,
+        ::surfaceVariant,
+        ::onSurfaceVariant,
+        ::inverseSurface,
+        ::inverseOnSurface,
+        ::outline,
+        ::outlineVariant,
+        ::shadow,
+        ::scrim,
+        ::surfaceTint,
+        ::primary,
+        ::onPrimary,
+        ::primaryContainer,
+        ::onPrimaryContainer,
+        ::inversePrimary,
+        ::secondary,
+        ::onSecondary,
+        ::secondaryContainer,
+        ::onSecondaryContainer,
+        ::tertiary,
+        ::onTertiary,
+        ::tertiaryContainer,
+        ::onTertiaryContainer,
+        ::error,
+        ::onError,
+        ::errorContainer,
+        ::onErrorContainer,
+        ::primaryFixed,
+        ::primaryFixedDim,
+        ::onPrimaryFixed,
+        ::onPrimaryFixedVariant,
+        ::secondaryFixed,
+        ::secondaryFixedDim,
+        ::onSecondaryFixed,
+        ::onSecondaryFixedVariant,
+        ::tertiaryFixed,
+        ::tertiaryFixedDim,
+        ::onTertiaryFixed,
+        ::onTertiaryFixedVariant,
+        ::controlActivated,
+        ::controlNormal,
+        ::controlHighlight,
+        ::textPrimaryInverse,
+        ::textSecondaryAndTertiaryInverse,
+        ::textPrimaryInverseDisableOnly,
+        ::textSecondaryAndTertiaryInverseDisabled,
+        ::textHintInverse,
+    )
 }
