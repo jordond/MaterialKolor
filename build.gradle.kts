52be296052cabe4bba52db0d8230da4558fa3d4d
Jordon de Hoog <jordon.dehoog@gmail.com>
2026-01-13 12:29:06 -0500

    Migrate from Android library to Multiplatform Android library (#453)

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,7 +3,7 @@ plugins {
     alias(libs.plugins.multiplatform) apply false
     alias(libs.plugins.compose) apply false
     alias(libs.plugins.compose.compiler) apply false
-    alias(libs.plugins.android.library) apply false
+    alias(libs.plugins.multiplatform.android.library) apply false
     alias(libs.plugins.kotlin.android) apply false
     alias(libs.plugins.poko) apply false
     alias(libs.plugins.dokka)
